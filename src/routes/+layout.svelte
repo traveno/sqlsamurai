4936055ed22a676311f72789990a93f8f039870e
Stephen <teamgreensf@gmail.com>
2023-07-31 21:15:14 -0700

    Update entity table names

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -122,7 +122,7 @@
 
     <SidebarTooltip>
       <div slot="text" class="select-none flex flex-row justify-center items-center h-full gap-2"><span>???</span></div>
-      <div role="none" class="cursor-pointer text-pink-500 flex flex-row justify-center items-center h-14 w-20 hover:bg-base-100 rounded-lg hover:rounded-r-none transition" on:click={() => $confettiMachine.addConfetti()}>
+      <div role="none" class="cursor-pointer text-pink-500 flex flex-row justify-center items-center h-14 w-20 hover:bg-slate-700 rounded-lg hover:rounded-r-none transition" on:click={() => $confettiMachine.addConfetti()}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8"><circle cx="9" cy="7" r="2"/><path d="M7.2 7.9 3 11v9c0 .6.4 1 1 1h16c.6 0 1-.4 1-1v-9c0-2-3-6-7-8l-3.6 2.6"/><path d="M16 13H3"/><path d="M16 17H3"/></svg>
       </div>
     </SidebarTooltip>
--- src/routes/equipment/+page.svelte
+++ src/routes/equipment/+page.svelte
@@ -5,8 +5,7 @@
     import NewItemMenu from "$lib/menus/NewItemMenu.svelte";
     import Paginate from "$lib/utils/Paginate.svelte";
 
-  let entity = 'equipment';
-  let entitySingle = 'equipment';
+  let entity = 'Equipment';
   let array = equipment;
   let currentId = 0;
   let lowerIndex = 0;
@@ -20,7 +19,7 @@
       <div class="font-mono text-3xl">{entity}</div>
       <div class="flex-grow"></div>
       <NewItemMenu>
-        <span slot="entity">equipment</span>
+        <span slot="entity">{entity}</span>
         <button class="btn btn-primary btn-sm">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
           New
@@ -43,7 +42,7 @@
         </div>
       </NewItemMenu>
       <DeleteItemMenu disabled={selectableArray.filter(i => i.checked).length === 0} queued={selectableArray.filter(a => a.checked).map(a => `${a.obj.sword_type} (${a.obj.equipment_id})`)}>
-        <span slot="entity">equipment</span>
+        <span slot="entity">{entity}</span>
         <button class="btn btn-error btn-outline btn-sm" disabled={selectableArray.filter(i => i.checked).length === 0}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>
           Delete
@@ -75,7 +74,7 @@
             {/each}
             <td class="flex flex-row justify-end">
               <NewItemMenu placement="bottom-end" mode={'edit'}>
-                <span slot="entity">equipment</span>
+                <span slot="entity">{entity}</span>
                 <button class="btn btn-ghost btn-sm text-neutral-content/50">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil"><path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/><path d="m15 5 4 4"/></svg>
                 </button>
